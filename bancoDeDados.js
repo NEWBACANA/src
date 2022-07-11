a2b8de0444175fdf75cf55e9e10f50b82a59666d
NEWBACANA <luizedsan5@gmail.com>
2022-07-11 14:09:34 -0300

    first commit

---
+++ bancoDeDados.js
@@ -0,0 +1,20 @@
+const sequence = {
+    _id: 1,
+    get id() { return this.id++}
+}
+
+const produtos = {}
+
+function salvarProduto(produto) {
+    if (!produto.id) produto.id = sequence.id
+    produtos[produto.id] = produto
+    return produto
+}
+
+function getproduto(id) {
+    return produtos[id] || {}
+}
+function getprodutos(){
+    return Object.values(produtos)
+}
+module.exports = { salvarProduto, getproduto, getprodutos}
